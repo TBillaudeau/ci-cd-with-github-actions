bf868ee566ebd2344e5be0d7898a6746076f27ee
Thomas <47186265+TBillaudeau@users.noreply.github.com>
2024-01-11 11:26:20 +0100

    Update Dockerfile to install Chrome and ChromeDriver

--- test-integration-app.py
+++ test-integration-app.py
@@ -8,8 +8,11 @@ from selenium.webdriver.common.by import By
 class TestAppE2E(unittest.TestCase):
     def setUp(self):
         # Launch your flask app first
-        chrome_options = Options()
-        self.driver = webdriver.Chrome(options=chrome_options)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
         self.driver.get('http://localhost:5000')
 
     def test_add_and_delete_item(self):
